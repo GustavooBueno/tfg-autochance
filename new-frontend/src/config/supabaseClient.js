db49452a3913524a4195918be9c8cc42d02122f2
Gustavo Bueno <gustavo.ibis.gb@gmail.com>
2025-04-06 01:14:15 -0300

    Correcao credenciais supabaseClient.js

--- new-frontend/src/config/supabaseClient.js
+++ new-frontend/src/config/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL || 'https://ansjxhspngkttwsxnwjt.supabase.co';
-const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFuc2p4aHNwbmdrdHR3c3hud2p0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI4NTYxMTcsImV4cCI6MjA1ODQzMjExN30.zXShD_yUtIlz97jZDSsOVfkP3F9OcHlHL0KwoR4pvfg';
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL 
+const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY
 
 // Log para confirmar a configuração do cliente
 console.log('Iniciando cliente Supabase com URL:', supabaseUrl);
